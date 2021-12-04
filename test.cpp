2aece43e99638b6d84f27883e230cc24b4c6befe
Craze-H <70187423+Craze-H@users.noreply.github.com>
2021-12-04 14:12:26 +0800

    \t

--- test.cpp
+++ test.cpp
@@ -6,9 +6,9 @@ void calculate();
 void printRegister(int n);
 int now_pos;
 int main(){
-    //freopen("in.txt", "r", stdin);
+    freopen("in.txt", "r", stdin);
 	getSym();
-    printf("ret i32 0\n");
+    printf("\tret i32 0\n");
     printf("}\n");
     return 0;
     words[0].id = 0;
